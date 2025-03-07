30cbb68bb54d6593f22a32c1bc9862f269773c06
DE ALMEIDA BATISTA Ranayana (DCH/MST) <ranayana.de.almeida.batista.ext@proximus.com>
2025-03-07 16:17:42 +0100

    Update cases for background change

--- src/utils/weatherBackground.ts
+++ src/utils/weatherBackground.ts
@@ -4,7 +4,7 @@ export const getWeatherBackground = (conditionCode: any) => {
       return require('../assets/sunny.jpg');
     case 1003: // Partly cloudy
       return require('../assets/partly-cloudy.jpg');
-    case 1006: // Cloudygit 
+    case 1006: // Cloudy
     case 1009: // Overcast
       return require('../assets/cloudy.jpg');
     case 1030: // Mist
@@ -23,7 +23,10 @@ export const getWeatherBackground = (conditionCode: any) => {
     case 1240: // Light rain shower
     case 1243: // Moderate or heavy rain shower
     case 1246: // Torrential rain shower
-      return require('../assets/rainy.jpg');
+    case 1237: // Ice pellets
+    case 1261: // Light showers of ice pellets
+    case 1264: // Moderate or heavy showers of ice pellets
+      return require('../assets/rain.jpg');
     case 1066: // Patchy snow possible
     case 1114: // Blowing snow
     case 1117: // Blizzard
@@ -35,7 +38,6 @@ export const getWeatherBackground = (conditionCode: any) => {
     case 1225: // Heavy snow
     case 1255: // Light snow showers
     case 1258: // Moderate or heavy snow showers
-      return require('../assets/snow.jpg');
     case 1069: // Patchy sleet possible
     case 1072: // Patchy freezing drizzle possible
     case 1168: // Freezing drizzle
@@ -46,17 +48,13 @@ export const getWeatherBackground = (conditionCode: any) => {
     case 1207: // Moderate or heavy sleet
     case 1249: // Light sleet showers
     case 1252: // Moderate or heavy sleet showers
-      return require('../assets/sleet.jpg');
+      return require('../assets/snow.jpg');
     case 1087: // Thundery outbreaks possible
     case 1273: // Patchy light rain with thunder
     case 1276: // Moderate or heavy rain with thunder
     case 1279: // Patchy light snow with thunder
     case 1282: // Moderate or heavy snow with thunder
       return require('../assets/thunder.jpg');
-    case 1237: // Ice pellets
-    case 1261: // Light showers of ice pellets
-    case 1264: // Moderate or heavy showers of ice pellets
-      return require('../assets/hail.jpg');
     default:
       return require('../assets/default-background.jpg');
   }
