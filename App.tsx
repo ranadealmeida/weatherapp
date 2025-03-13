c214d8518bc08cb3083b749170f29e6923dce7ed
Radad El Jaidi <radad1998@gmail.com>
2025-03-13 13:17:29 +0100

    Use the onSwipeRight and onSwipeLeft of GestureRecognizer instead of the gesture handler and and rn reanimated

--- App.tsx
+++ App.tsx
@@ -5,21 +5,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import FavouritesScreen from './src/screens/FavouritesScreen';
 import { WeatherProvider } from './src/context/WeatherContext';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <WeatherProvider>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
-            <Stack.Screen name="Favourites" options={{ headerTitle: '' }} component={FavouritesScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </WeatherProvider>
-    </GestureHandlerRootView>
+    <WeatherProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+          <Stack.Screen name="Favourites" options={{ headerTitle: '' }} component={FavouritesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </WeatherProvider>
   );
 };
 
